44fc474b4093873739632b5c188ea66bf8c960ff
Enzo Sandré <atelier@enzosandre.fr>
2023-11-15 13:42:26 +0100

    1 -> I

---
+++ src/ConvertissuerNombresRomains.py
@@ -0,0 +1,4 @@
+class ConvertisseurNombresRomains:
+    @classmethod
+    def convertir(cls, chiffre_arabe):
+        return "I"
--- test/testRomanNumbers.py
+++ test/testRomanNumbers.py
@@ -1,9 +1,18 @@
 import unittest
 
+from ConvertissuerNombresRomains import ConvertisseurNombresRomains
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    def test_un(self):
+        # ETANT DONNE le chiffre 1
+        chiffre = 1
+
+        # QUAND on le convertit en nombres romains
+        nombres_romains = ConvertisseurNombresRomains.convertir(chiffre)
+
+        # ALORS on obtient "I"
+        self.assertEqual("I", nombres_romains)
 
 
 if __name__ == '__main__':
