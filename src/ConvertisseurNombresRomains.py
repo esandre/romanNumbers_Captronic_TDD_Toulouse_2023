c5a95633771e67be0c9e20c774d17153a67d1821
Enzo Sandré <atelier@enzosandre.fr>
2023-11-15 15:10:25 +0100

    Refacto

--- src/ConvertisseurNombresRomains.py
+++ src/ConvertisseurNombresRomains.py
@@ -3,11 +3,15 @@ class ConvertisseurNombresRomains:
     def convertir(cls, chiffre_arabe):
         if (chiffre_arabe + 1) % 5 == 0:
             suivant = cls.convertir(chiffre_arabe + 1)
-            if len(suivant) == 2:
-                return suivant[0] + "I" + suivant[1]
-            return "I" + suivant
+            return cls.__insert_between(suivant, "I")
         if chiffre_arabe >= 10:
             return "X" + cls.convertir(chiffre_arabe - 10)
         elif chiffre_arabe >= 5:
             return "V" + cls.convertir(chiffre_arabe - 5)
         return "I" * chiffre_arabe
+
+    @classmethod
+    def __insert_between(cls, string, insertion):
+        if len(string) == 2:
+            return string[0] + insertion + string[1]
+        return insertion + string
