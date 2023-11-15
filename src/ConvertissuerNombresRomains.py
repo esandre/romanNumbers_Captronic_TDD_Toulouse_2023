9677d8eab88ffd5d522862c32ea9b58d78d57720
Enzo Sandré <atelier@enzosandre.fr>
2023-11-15 14:36:01 +0100

    refacto de la conversion

--- src/ConvertissuerNombresRomains.py
+++ src/ConvertissuerNombresRomains.py
@@ -1,10 +1,10 @@
 class ConvertisseurNombresRomains:
     @classmethod
     def convertir(cls, chiffre_arabe):
-        if chiffre_arabe == 4:
-            return "IV"
-        elif chiffre_arabe == 9:
+        if chiffre_arabe == 9:
             return "IX"
         elif chiffre_arabe >= 5:
             return "V" + cls.convertir(chiffre_arabe - 5)
+        elif chiffre_arabe == 4:
+            return "IV"
         return "I" * chiffre_arabe
